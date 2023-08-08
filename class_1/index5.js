6644a2bcef1ee15b947c99f81945700f7b6ce76c
chandu-0214 <ncr02141997@gmail.com>
2023-08-08 20:23:02 +0530

    secound commit

---
+++ class_1/index5.js
@@ -0,0 +1,57 @@
+// Loops (for,while)
+// console.log("chandu")
+// console.log("chandu")
+// console.log("chandu")
+// console.log("chandu")
+// console.log("chandu")
+
+// basic for loop
+
+// syntax => for(initialization,termination condition,updation){
+                            // code
+//             }
+
+for(let i=0;i<10;i++){
+    // console.log(i)
+}
+
+// for loop in arrays 
+
+let students=['xyz','Disha panigrahi','sheela singh','Pema Khandu']
+
+// for(let eachStudent of students){
+//     console.log(eachStudent)
+// }
+students.push('chandu')
+
+let lengthOfTheStudentsArray= students.length
+for(let i=0;i<lengthOfTheStudentsArray;i++){
+    console.log(i)
+    console.log(students[i])
+}
+console.log(students)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
