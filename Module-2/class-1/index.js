a9da1efe8d51a8087d465df79dea3c297cdfe96b
chandu-0214 <ncr02141997@gmail.com>
2023-08-24 19:00:12 +0530

    dom1 commit

---
+++ Module-2/class-1/index.js
@@ -0,0 +1,19 @@
+console.log("Hello Dom")
+
+let headingElemt= document.getElementById('heading')
+headingElemt.textContent='Hello Dom'
+headingElemt.style.color='red'
+headingElemt.style.backgroundColor='yellow'
+headingElemt.className='heading'
+console.log(headingElemt)
+
+let paragraphEl = document.getElementById('paragraph')
+
+console.log(paragraphEl)
+
+
+
+
+
+
+
