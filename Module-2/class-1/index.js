fd1de17633005e96a997bd7e279ac66620901fec
chandu-0214 <ncr02141997@gmail.com>
2023-08-25 18:52:07 +0530

    dom2 commit

--- Module-2/class-1/index.js
+++ Module-2/class-1/index.js
@@ -1,15 +1,36 @@
-console.log("Hello Dom")
 
 let headingElemt= document.getElementById('heading')
-headingElemt.textContent='Hello Dom'
-headingElemt.style.color='red'
-headingElemt.style.backgroundColor='yellow'
-headingElemt.className='heading'
-console.log(headingElemt)
+let containerElement = document.getElementById('container')
+
+
+//console.log(headingElemt)
 
 let paragraphEl = document.getElementById('paragraph')
 
-console.log(paragraphEl)
+//console.log(paragraphEl)
+
+// creates  an events 
+let buttonElement = document.getElementById("clickButton")
+
+const handleClickButton =()=>{
+    console.log("Button is clicked")
+    headingElemt.textContent='Hello Dom'
+headingElemt.style.color='red'
+headingElemt.style.backgroundColor='yellow'
+headingElemt.className='heading'
+paragraphEl.textContent="Yeahhhh I changes"
+let headingElemt2 = document.createElement('p')
+containerElement.appendChild(headingElemt2)
+headingElemt2.textContent="Created from JS"
+}
+buttonElement.addEventListener('click',handleClickButton)
+
+// creat a new html elements 
+
+
+
+
+
 
 
 
