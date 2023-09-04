de68b0162aade5da0979a1eb9b12012e4982a008
chandu-0214 <ncr02141997@gmail.com>
2023-09-04 19:13:57 +0530

    dom6 commit

--- Module-2/wiki application/index.js
+++ Module-2/wiki application/index.js
@@ -1,17 +1,70 @@
-console.log("Data fetching start")
-let url = "https://restcountries.com/v2/all"
+// console.log("Data fetching start")
+// let url = "https://restcountries.com/v2/all"
 
-// fetch()
+// // fetch()
 
-fetch(url)
+// fetch(url)
+// .then((res)=>res.json())
+// .then((data)=>console.log("Data",data))
+
+// console.log("Data Recieved")
+
+let searchInputEl = document.getElementById('searchInput')
+let resultsContainer = document.getElementById('resultsContainer')
+let wikiURL = "https://apis.ccbp.in/wiki-search?search=";
+
+
+
+const integrateItem = (eachItem)=>{
+    let divElemt = document.createElement('div')
+    let h2Elemt = document.createElement('h2')
+    let acchorElemt = document.createElement('a')
+    let descriptionElemt = document.createElement('p')
+
+    h2Elemt.textContent=eachItem.title
+    divElemt.appendChild(h2Elemt)
+
+    acchorElemt.textContent=eachItem.link
+    acchorElemt.href=eachItem.link
+    divElemt.appendChild(acchorElemt)
+
+    descriptionElemt.textContent=eachItem.description
+    divElemt.appendChild(descriptionElemt)
+
+    resultsContainer.appendChild(divElemt)
+
+}
+
+
+const renderData = (data)=>{
+    resultsContainer.textContent=''
+    for(let eachItem of data.search_results){
+        integrateItem(eachItem)
+    }
+
+
+}
+const fetchdata = (searchvalue)=>{
+   
+    let url = wikiURL + searchvalue
+    fetch(url)
 .then((res)=>res.json())
-.then((data)=>console.log("Data",data))
+.then((data)=>renderData(data))
+    
 
-console.log("Data Recieved")
+}
 
 
 
+const handlesearchInput = (event)=>{
+    if(event.target.value!=="" &&event.key==='Enter'){
+    fetchdata(event.target.value)
+    }
 
+
+}
+
+searchInputEl.addEventListener('keyup',handlesearchInput)
 
 
 
